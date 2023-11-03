398fb6514c12d6c94cebea601ddc622111de9450
Andreas Kohlbecker <kohlbecker@zkm.de>
2023-11-03 09:22:36 +0100

    fix #8 scaling pageW and pageH to backgroundResolution

--- xpdf/JSONGen.cc
+++ xpdf/JSONGen.cc
@@ -389,9 +389,11 @@ int JSONGen::convertPage(
         png_destroy_write_struct(&png, &pngInfo);
     }
 
-    // page size
-    pageW = doc->getPageCropWidth(pg);
-    pageH = doc->getPageCropHeight(pg);
+    // page size (always in 72 dpi!!!)
+    double defaultDPI = 72;
+    double scaleFactor  = backgroundResolution / defaultDPI;
+    pageW = doc->getPageCropWidth(pg) * scaleFactor;
+    pageH = doc->getPageCropHeight(pg) * scaleFactor;
     
     // get the PDF text
     doc->displayPage(textOut, pg, backgroundResolution, backgroundResolution, 0, gFalse, gTrue, gFalse);
